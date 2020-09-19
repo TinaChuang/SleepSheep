2bcc438252d6df6f32677121f3281318e454b7b1
Tina <st3112018@gmail.com>
2020-09-19 17:24:32 +0800

    move image

--- final_project/src/views/frontend/Index.vue
+++ final_project/src/views/frontend/Index.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
     <loading :active.sync="isLoading"/>
-    <div class="section_1">
+    <div class="section_1 d-flex align-items-center">
       <div class="container">
-        <div class="row justify-content-around">
-          <div class="col-12 col-md-3">
+        <div class="row">
+          <div class="col-12 col-md-6">
             <h1 style="font-size: 4rem;">眠綿羊</h1>
             <h3 style="font-size: 2rem;">Sleep sheep</h3>
             <p style="font-size: 1.5rem;">sleep wll with you</p>
-            <router-link class="d-block btn btn-pink" to="/products">
+            <router-link class="btn btn-pink px-5" to="/products">
               眠綿羊群
             </router-link>
           </div>
-          <div class="col-12 col-md-9"></div>
+          <div class="col-12 col-md-6">
+            <img class="img-fluid" src="@/assets/image/sheep-1.svg" alt="Sleep-Sheep" title="Sleep-Sheep">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section_2">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <img class="img-fluid" src="@/assets/image/sheep-2.svg" alt="Sleep-Sheep" title="Sleep-Sheep">
+          </div>
+          <div class="col-12 col-md-6"></div>
+        </div>
+      </div>
+    </div>
+    <div class="section_3"></div>
+    <div class="section_4"></div>
+    <div class="section_5">
+      <div class="container">
+        <div class="row">
+          <div class="col-12"></div>
         </div>
       </div>
     </div>
